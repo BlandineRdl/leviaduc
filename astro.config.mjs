5fae654e66dc90fcf06aa4ef23cc5ae006c2d1d6
WildCöde <brondel@protonmail.com>
2024-01-04 10:52:40 +0100

    add github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  site: "https://blandinerdl.github.io",
+  base: "/leviaduc",
 });
